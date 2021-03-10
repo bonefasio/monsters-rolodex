d5044491b80f75a9fcb98db5fdd3e8e8bc1d99f4
Bonefasio Lemaki <lbonefasio@gmail.com>
2021-03-10 16:13:31 +1200

    Updates

---
+++ precache-manifest.0a114cef45340a2590d3fbb938aeba6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65f0d65c46b230ec5ddf057d34a3fbd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d0bc8383626c3962ef45",
+    "url": "/monsters-rolodex/static/css/main.ac03ac47.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0bc8383626c3962ef45",
+    "url": "/monsters-rolodex/static/js/main.80efbed7.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
